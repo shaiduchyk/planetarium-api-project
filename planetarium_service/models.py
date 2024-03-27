6983d454012d6ed539f931b405c6477ad9169587
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-27 19:38:43 +0200

    chore: added static method for ticket validate.

--- planetarium_service/models.py
+++ planetarium_service/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
+from rest_framework.exceptions import ValidationError
 
 
 class CustomUser(AbstractUser):
@@ -66,27 +67,48 @@ class Ticket(models.Model):
         Reservation, on_delete=models.CASCADE, related_name="tickets"
     )
 
+    @staticmethod
+    def validate_ticket(row, seat, planetarium_dome, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(planetarium_dome, cinema_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                                          f"number must be in available range: "
+                                          f"(1, {cinema_hall_attr_name}): "
+                                          f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
     def __str__(self):
         return (
             f"{str(self.show_session)} (row: {self.row}, seat: {self.seat})"
         )
 
-    @staticmethod
-    def validate_ticket(row, seat, show_session, error_to_raise):
-        planetarium_dome = show_session.planetarium_dome
-        for (ticket_attr_value, ticket_attr_name,
-             planetarium_dome_attr_name) in [
-            (row, "row", "rows"),
-            (seat, "seat", "seats_in_row"),
-        ]:
-            count_attrs = getattr(planetarium_dome, planetarium_dome_attr_name)
-            if not (1 <= ticket_attr_value <= count_attrs):
-                raise error_to_raise(
-                    {
-                        ticket_attr_name:
-                            f"{ticket_attr_name} "
-                            f"number must be in available range: "
-                            f"(1, {planetarium_dome_attr_name}): "
-                            f"(1, {count_attrs})"
-                    }
-                )
+    class Meta:
+        unique_together = ("show_session", "row", "seat")
+        ordering = ["row", "seat"]
